0c17fc996808765039e9116768bc979e3a719c5f
5j9 <5j9@users.noreply.github.com>
2025-08-07 22:58:01 +0330

    remove the print statement

--- devtools_sitecustomize/__init__.py
+++ devtools_sitecustomize/__init__.py
@@ -6,10 +6,4 @@ from devtools import debug
 
 
 def add_debug_to_builtins():
-    """
-    This function adds devtools.debug to builtins.
-    """
-    print(
-        '🚀 devtools.debug being added to builtins via devtools-sitecustomize plugin!'
-    )
     setattr(builtins, 'debug', debug)
